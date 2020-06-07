015af388acc7be001093fd95ba2386725f6266a6
TrymTv <tiltrym@gmail.com>
2020-06-07 20:55:30 +0200

    Remove directories in .gitignore

---
+++ src/maze/MazeMaker.java
@@ -0,0 +1,12 @@
+package maze;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class MazeMaker {
+	private static LinkedList<int[]> queue = new LinkedList<>();
+	
+	private static void make(String targetPath, int size) {
+		File outputFile = new File(targetPath);
+	}
+}
